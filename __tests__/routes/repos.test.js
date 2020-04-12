2c46c19aec15fbf7b3c97c90b2cd54383efb49ff
edsonha <edsonha@gmail.com>
2020-04-12 22:11:42 +0800

    [Ed] Add test for mock axios to return 500 error

--- __tests__/routes/repos.test.js
+++ __tests__/routes/repos.test.js
@@ -29,4 +29,19 @@ describe("GET /repos", () => {
       "https://api.github.com/search/repositories?q=/react"
     );
   });
+
+  it("should return 500 and error message when there is error", async () => {
+    const error = new Error("Error: Request failed with status code 500");
+    moxios.stubRequest(/api.github.com\/search\/repositories\?q=\//, {
+      status: 500,
+      response: { error },
+    });
+    const response = await request(app).get("/repos");
+
+    expect(response.status).toBe(500);
+    expect(response.body.message).toBe("Request failed with status code 500");
+    expect(moxios.requests.mostRecent().url).toBe(
+      "https://api.github.com/search/repositories?q=/react"
+    );
+  });
 });

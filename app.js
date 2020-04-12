d5b32cb1c6bfa2f1b507d4692c20c94aa048c0a8
edsonha <edsonha@gmail.com>
2020-04-12 15:46:57 +0800

    [Ed] Install cors - enable all CORS requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json("Welcome to Express REST API");

ff61e813e2cc50713d96f242a99879f6a322bca9
edsonha <edsonha@gmail.com>
2020-04-12 21:55:28 +0800

    [Ed] Install moxios to create mock axios for testing API response

--- __tests__/routes/repos.test.js
+++ __tests__/routes/repos.test.js
@@ -1,10 +1,32 @@
 const app = require("../../app");
 const request = require("supertest");
+const moxios = require("moxios");
 
-xdescribe("GET /repos", () => {
-  it("should return a welcome from repo", async () => {
+describe("GET /repos", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it("should mock fetch Github Search API as well as return 200 and response", async () => {
+    moxios.stubRequest(/api.github.com\/search\/repositories\?q=\//, {
+      status: 200,
+      response: {
+        suggestionArray: [],
+        resultArray: [],
+      },
+    });
     const response = await request(app).get("/repos");
+
     expect(response.status).toBe(200);
-    expect(response.body).toBe("Welcome from Repos Router");
+    expect(response.body).toEqual({
+      suggestionArray: [],
+      resultArray: [],
+    });
+    expect(moxios.requests.mostRecent().url).toBe(
+      "https://api.github.com/search/repositories?q=/react"
+    );
   });
 });
